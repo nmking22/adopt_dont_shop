11dcce42b1566dc0e673e094a325037ca8a4d078
Nick King <nickmaxking@gmail.com>
2020-10-07 20:53:26 -0600

    Update index to display all shelters and all shelter information

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,9 +1,26 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['Shelter 1', 'Shelter 2']
+    @shelters = Shelter.all
+  end
+
+  def new
   end
 
   def show
     @shelter = Shelter.find(params[:id])
+  end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip],
+      })
+
+    shelter.save
+
+    redirect_to '/shelters'
   end
 end
