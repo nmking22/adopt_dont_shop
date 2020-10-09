e96efc9b1a7d97ff57d873d39ac8072cf3ad376d
Nick King <nickmaxking@gmail.com>
2020-10-09 00:21:19 -0600

    Add test for user story 4

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -26,3 +26,45 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe "When I visit the Shelter Index page" do
+    it "Then I see a link to create a new Shelter, 'New Shelter'" do
+
+      visit '/shelters'
+
+      expect(page).to have_link("New Shelter")
+    end
+    describe "When I click this link" do
+      it "Then I am taken to '/shelters/new' where I see a form for a new shelter" do
+
+        visit '/shelters'
+        click_link("New Shelter")
+        expect(current_path).to eq '/shelters/new'
+        # binding.pry
+        # Need to fix has field expectations
+        # expect(page).to has_field?('name')
+        # expect(page).to has_button?('Create Shelter')
+      end
+      describe "When I fill out the form with a new shelter's name, address, city, state and zip And I click the button 'Create Shelter' to submit the form" do
+        it "Then a `POST` request is sent to '/shelters' and a new shelter is created" do
+          visit '/shelters'
+          click_link("New Shelter")
+          fill_in('name', :with => 'The Shell')
+          fill_in('address', :with => '123 ABC Ave.')
+          fill_in('city', :with => 'Denver')
+          fill_in('state', :with => 'CO')
+          fill_in('zip', :with => '88888')
+          click_button("Create Shelter")
+
+          expect(current_path).to eq '/shelters'
+          expect(page).to have_content("The Shell")
+          expect(page).to have_content("123 ABC Ave.")
+          expect(page).to have_content("Denver")
+          expect(page).to have_content("CO")
+          expect(page).to have_content("88888")
+        end
+      end
+    end
+  end
+end
