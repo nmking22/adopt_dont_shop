edee4c2d48bc344b608cd3f715738946deb57dab
Nick King <nickmaxking@gmail.com>
2020-10-10 13:47:41 -0600

    Add description and adoption_status columns to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_062826) do
+ActiveRecord::Schema.define(version: 2020_10_10_185239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_10_10_062826) do
     t.string "age"
     t.string "sex"
     t.string "location"
+    t.string "description"
+    t.string "adoption_status"
   end
 
   create_table "shelters", force: :cascade do |t|
