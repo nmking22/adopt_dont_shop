b5f0577d9e300938c227ebe852486a657a179bf8
Nick King <nickmaxking@gmail.com>
2020-10-11 13:15:40 -0600

    Add test and functionality for shelterr and pet index links at /shelters

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -143,3 +143,23 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe "When I visit '/shelters'" do
+    it "Then I see a link at the top of the page that takes me to the Shelter Index" do
+      visit '/shelters'
+
+      expect(page).to have_link('Shelters Index')
+    end
+  end
+end
+
+describe "As a visitor" do
+  describe "When I visit '/shelters'" do
+    it "Then I see a link at the top of the page that takes me to the Pet Index" do
+      visit '/shelters'
+
+      expect(page).to have_link('Pets Index')
+    end
+  end
+end
