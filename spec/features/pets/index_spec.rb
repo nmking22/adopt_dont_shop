298d745ee606d132bd56c6bc68da8eb9e15a823c
Nick King <nickmaxking@gmail.com>
2020-10-11 10:37:16 -0600

    Add tests for 'Delete Pet' link in pets index page

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -88,3 +88,54 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe "When I visit the pets index page or a shelter pets index page" do
+    it "Next to every pet, I see a link to delete that pet" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+      pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                         name: "Doc",
+                         description: "Golden Retriever",
+                         age: "4",
+                         sex: "M",
+                         adoption_status: "Adoptable",
+                         location: "#{shelter_1.name}",
+                         shelter_id: "#{shelter_1.id}")
+
+      visit '/pets'
+
+      expect(page).to have_link('Delete Pet')
+    end
+    describe "When I click the link" do
+      it "I should be taken to the pets index page where I no longer see that pet" do
+        shelter_1 = Shelter.create(name: "Shelter 1",
+                                   address: "123 Fake St.",
+                                   city: "Denver",
+                                   state: "CO",
+                                   zip: "88888")
+        pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                           name: "Doc",
+                           description: "Golden Retriever",
+                           age: "4",
+                           sex: "M",
+                           adoption_status: "Adoptable",
+                           location: "#{shelter_1.name}",
+                           shelter_id: "#{shelter_1.id}")
+
+        visit '/pets'
+
+        click_link('Delete Pet')
+        expect(current_path).to eq '/pets'
+        expect(page).to have_no_content("#{pet_1.name}")
+        expect(page).to have_no_content("#{pet_1.image}")
+        expect(page).to have_no_content("#{pet_1.description}")
+        expect(page).to have_no_content("#{pet_1.age}")
+        expect(page).to have_no_content("#{pet_1.sex}")
+      end
+    end
+  end
+end
