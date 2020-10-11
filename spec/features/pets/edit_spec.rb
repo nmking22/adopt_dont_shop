87c2136aab2a825a873af80dfe74ce1f08fd285b
Nick King <nickmaxking@gmail.com>
2020-10-11 13:33:12 -0600

    Add tests and functionality for linkds to pet and shelter index pages from pet edit page

--- spec/features/pets/edit_spec.rb
+++ spec/features/pets/edit_spec.rb
@@ -39,3 +39,53 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe "When I visit '/pets/:id/edit'" do
+    it "Then I see a link at the top of the page that takes me to the Shelter Index" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+
+      pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                               name: "Doc",
+                               description: "Golden Retriever",
+                               age: "4",
+                               sex: "male",
+                               adoption_status: "adoptable",
+                               location: "#{shelter_1.name}",
+                               shelter_id: "#{shelter_1.id}")
+
+      visit "/pets/#{pet_1.id}/edit"
+
+      expect(page).to have_link('Shelters Index')
+    end
+  end
+end
+
+describe "As a visitor" do
+  describe "When I visit '/pets/:id/edit'" do
+    it "Then I see a link at the top of the page that takes me to the Pet Index" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+
+      pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                               name: "Doc",
+                               description: "Golden Retriever",
+                               age: "4",
+                               sex: "male",
+                               adoption_status: "adoptable",
+                               location: "#{shelter_1.name}",
+                               shelter_id: "#{shelter_1.id}")
+
+      visit "/pets/#{pet_1.id}/edit"
+
+      expect(page).to have_link('Pets Index')
+    end
+  end
+end
