25acac55c03014d3f0c08b6c7e2b5f8eb0c17d3c
Nick King <nickmaxking@gmail.com>
2020-10-11 00:51:46 -0600

    Modify test setups to work with pet-shelter one to many relationship

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -3,20 +3,27 @@ require "rails_helper"
 describe "As a visitor" do
   describe "When I visit '/pets'" do
     it "Then I see each Pet in the system including the Pet's image, name, approximate age, sex and name of the shelter where the pet is currently located" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
       pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
                          name: "Doc",
                          description: "Golden Retriever",
                          age: "4",
                          sex: "M",
                          adoption_status: "Adoptable",
-                         location: "A1")
+                         location: "#{shelter_1.name}",
+                         shelter_id: "#{shelter_1.id}")
       pet_2 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
                          name: "Puppaloo",
                          description: "Golden Retriever",
                          age: "6",
                          sex: "F",
                          adoption_status: "Adoptable",
-                         location: "A2")
+                         location: "#{shelter_1.name}",
+                         shelter_id: "#{shelter_1.id}")
 
 
       visit '/pets'
