d9e5f8c16897c3bbb4247aba0fa7ed294798a973
Nick King <nickmaxking@gmail.com>
2020-10-11 10:31:59 -0600

    Add test for Update Pet link on pets index page'

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -36,3 +36,55 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe "When I visit the pets index page or a shelter pets index page" do
+    it "Next to every pet, I see a link to edit that pet's info" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+      pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                         name: "Doc",
+                         description: "Golden Retriever",
+                         age: "4",
+                         sex: "M",
+                         adoption_status: "Adoptable",
+                         location: "#{shelter_1.name}",
+                         shelter_id: "#{shelter_1.id}")
+
+      visit '/pets'
+
+      expect(page).to have_link('Update Pet')
+    end
+    describe "When I click the link" do
+      it "I should be taken to that pets edit page where I can update its information just like in User Story 11" do
+        shelter_1 = Shelter.create(name: "Shelter 1",
+                                   address: "123 Fake St.",
+                                   city: "Denver",
+                                   state: "CO",
+                                   zip: "88888")
+        pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                           name: "Doc",
+                           description: "Golden Retriever",
+                           age: "4",
+                           sex: "M",
+                           adoption_status: "Adoptable",
+                           location: "#{shelter_1.name}",
+                           shelter_id: "#{shelter_1.id}")
+
+        visit '/pets'
+
+        click_link('Update Pet')
+        expect(current_path).to eq "/pets/#{pet_1.id}/edit"
+        expect(page).to have_field('name')
+        expect(page).to have_field('image')
+        expect(page).to have_field('description')
+        expect(page).to have_field('age')
+        expect(page).to have_field('sex')
+        expect(page).to have_button('Update Pet')
+      end
+    end
+  end
+end
