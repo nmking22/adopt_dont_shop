8d9e81ba3eb50a73d47bc1b19ad8c8543ff04b0f
Nick King <nickmaxking@gmail.com>
2020-10-11 01:57:09 -0600

    Add test for update shelter links in shelter index (user story 13)

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -8,11 +8,11 @@ describe "As a visitor" do
                                  city: "Denver",
                                  state: "CO",
                                  zip: "88888")
-      shelter_2 = Shelter.create(name: "Shelter 1",
-                                 address: "123 Fake St.",
-                                 city: "Denver",
+      shelter_2 = Shelter.create(name: "Shelter 2",
+                                 address: "1234 Fake St.",
+                                 city: "Vail",
                                  state: "CO",
-                                 zip: "88888")
+                                 zip: "81888")
 
       visit '/shelters'
 
@@ -40,7 +40,7 @@ describe "As a visitor" do
 
         visit '/shelters'
         click_link("New Shelter")
-        
+
         expect(current_path).to eq '/shelters/new'
         expect(page).to have_field('name')
         expect(page).to have_field('address')
@@ -67,6 +67,42 @@ describe "As a visitor" do
           expect(page).to have_content("CO")
           expect(page).to have_content("88888")
         end
+      end
+    end
+  end
+end
+
+describe "As a visitor" do
+  describe "When I visit the shelter index page" do
+    it "Next to every shelter, I see a link to edit that shelter's info" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+
+      visit '/shelters'
+      expect(page).to have_link('Update Shelter')
+    end
+    describe "When I click the link" do
+      it "I should be taken to that shelters edit page where I can update its information just like in User Story 5" do
+        shelter_1 = Shelter.create(name: "Shelter 1",
+                                   address: "123 Fake St.",
+                                   city: "Denver",
+                                   state: "CO",
+                                   zip: "88888")
+
+        visit '/shelters'
+
+        click_link('Update Shelter')
+
+        expect(current_path).to eq "/shelters/#{shelter_1.id}/edit"
+        expect(page).to have_field('name')
+        expect(page).to have_field('address')
+        expect(page).to have_field('city')
+        expect(page).to have_field('state')
+        expect(page).to have_field('zip')
+        expect(page).to have_link('Update Shelter')
       end
     end
   end
