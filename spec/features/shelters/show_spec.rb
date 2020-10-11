04dfeae5f5b565ff4e269644d5aafd5b45409d06
Nick King <nickmaxking@gmail.com>
2020-10-11 13:28:34 -0600

    Add tests and functionality for links to shelter and pet index pages from shelter show page

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -55,3 +55,35 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe "When I visit a shelter show page" do
+    it "Then I see a link at the top of the page that takes me to the Shelter Index" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+
+      visit "/shelters/#{shelter_1.id}"
+
+      expect(page).to have_link('Shelters Index')
+    end
+  end
+end
+
+describe "As a visitor" do
+  describe "When I visit a shelter show page" do
+    it "Then I see a link at the top of the page that takes me to the Pet Index" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+
+      visit "/shelters/#{shelter_1.id}"
+
+      expect(page).to have_link('Pets Index')
+    end
+  end
+end
