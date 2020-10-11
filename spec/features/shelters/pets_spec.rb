3c6d2fbeb4725fa48f9ff8867028d2e3086c2232
Nick King <nickmaxking@gmail.com>
2020-10-10 18:06:22 -0600

    Add test for shelter pets view (user story 8)

---
+++ spec/features/shelters/pets_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+describe "As a visitor" do
+  describe "When I visit '/shelters/:shelter_id/pets'" do
+    it "Then I see each Pet that can be adopted from that Shelter with that shelter_id including the Pet's image, name, age and sex" do
+      shelter_1 = Shelter.create(name: "Shelter 1",
+                                 address: "123 Fake St.",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "88888")
+      pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                         name: "Doc",
+                         description: "Golden Retriever",
+                         age: "4",
+                         sex: "M",
+                         adoption_status: "Adoptable",
+                         location: "Shelter 1",
+                         shelter_id: shelter_1.id)
+      pet_2 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                         name: "Puppaloo",
+                         description: "Golden Retriever",
+                         age: "6",
+                         sex: "F",
+                         adoption_status: "Adoptable",
+                         location: "Shelter 1",
+                         shelter_id: shelter_1.id)
+
+      visit "/shelters/#{shelter_1.id}/pets"
+
+      expect(page).to have_content("#{pet_1.name}")
+      expect(page).to have_content("#{pet_2.image}")
+      expect(page).to have_content("#{pet_1.age}")
+      expect(page).to have_content("#{pet_2.sex}")
+    end
+  end
+end
